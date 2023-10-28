7c8b2e46ad60d5c413895f50ccbcbbf0918837c4
TyT_MacuK <koroza1995@gmail.com>
2023-10-28 11:59:57 +0300

    module 3

---
+++ src/main/java/course/concurrency/exams/auction/AuctionOptimistic.java
@@ -0,0 +1,27 @@
+package course.concurrency.exams.auction;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class AuctionOptimistic implements Auction {
+
+    private Notifier notifier;
+
+    public AuctionOptimistic(Notifier notifier) {
+        this.notifier = notifier;
+    }
+
+    private Bid latestBid;
+
+    public boolean propose(Bid bid) {
+        if (bid.getPrice() > latestBid.getPrice()) {
+            notifier.sendOutdatedMessage(latestBid);
+            latestBid = bid;
+            return true;
+        }
+        return false;
+    }
+
+    public Bid getLatestBid() {
+        return latestBid;
+    }
+}
