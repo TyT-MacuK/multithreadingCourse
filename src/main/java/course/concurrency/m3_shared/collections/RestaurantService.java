f034f2804aacbbacf6f9f0920d8f32cb9abee358
TyT_MacuK <koroza1995@gmail.com>
2023-11-06 13:50:48 +0300

    module 3. less 11. RestaurantService

--- src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
+++ src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
@@ -1,9 +1,9 @@
 package course.concurrency.m3_shared.collections;
 
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 public class RestaurantService {
 
@@ -13,7 +13,11 @@ public class RestaurantService {
         put("C", new Restaurant("C"));
     }};
 
-    private Object stat;
+    private Map<String, Long> stat = new ConcurrentHashMap<>() {{
+        put("A", 0L);
+        put("B", 0L);
+        put("C", 0L);
+    }};
 
     public Restaurant getByName(String restaurantName) {
         addToStat(restaurantName);
@@ -21,11 +25,10 @@ public class RestaurantService {
     }
 
     public void addToStat(String restaurantName) {
-        // your code
+        stat.merge(restaurantName, 1L, (k,v) -> k + 1);
     }
 
     public Set<String> printStat() {
-        // your code
-        return new HashSet<>();
+        return stat.entrySet().stream().map(val -> val.getKey() + " - " + val.getValue()).collect(Collectors.toSet());
     }
 }
